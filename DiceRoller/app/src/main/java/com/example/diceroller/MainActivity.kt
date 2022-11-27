126ff013a7938c3d85f02fd351b4e41084ebe48f
Kevin Kuan <kevin.kuan.learning@gmail.com>
2022-11-27 10:26:03 +0800

    Use view binding to inflate
    However this changes the layout.
    
    Reference: https://developer.android.com/topic/libraries/view-binding
    Problem: https://stackoverflow.com/questions/61695769/why-using-view-binding-is-changing-the-layout

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding /* activity_main.xml */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
+        binding.rollButton.text = "Let's Binding!"
     }
 }
